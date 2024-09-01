2603cebbb27b24c19dcea233bd8e4a9d4feabaef
Michael Bakker <m.bakker@draaijerpartners.nl>
2024-09-01 10:29:39 +0200

    Minimize to system tray. Don't move mouse when user has moved it during the last interval.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using Timer = System.Threading.Timer;
 
 namespace Mauzer;
 
@@ -29,7 +30,6 @@ internal partial class Program
     [Flags]
     public enum EXECUTION_STATE : uint
     {
-        ES_AWAYMODE_REQUIRED = 0x00000040,
         ES_CONTINUOUS = 0x80000000,
         ES_DISPLAY_REQUIRED = 0x00000002,
         ES_SYSTEM_REQUIRED = 0x00000001
@@ -62,45 +62,84 @@ internal partial class Program
     [return: MarshalAs(UnmanagedType.Bool)]
     private static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
+    private static NotifyIcon? tray;
+
     #endregion
 
     private static void Main()
     {
-        var moveTimer = new Timer(new Mover().Move, null, 3000, 10000);
+        var mover = new Mover(GetConsoleWindow());
+        var moveTimer = new Timer(mover.Move, null, 3000, 5000);
 
         SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED); // prevent Idle-to-Sleep
-
+        tray = new NotifyIcon
+        {
+            Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+            Text = Console.Title,
+            BalloonTipTitle = Console.Title,
+            BalloonTipText = Console.Title,
+            Visible = true,
+        };
+        tray.DoubleClick += (s, e) => {
+            mover.ToggleWindow(true);
+            Console.ReadKey();
+            moveTimer.Dispose();
+            tray.Dispose();
+            Environment.ExitCode = 0;
+            Application.Exit();
+        };
+        mover.ToggleWindow(true);
         Console.WriteLine("Place mouse over the target within 3 seconds.");
         Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
-
-        moveTimer.Dispose();
+        Application.Run();
     }
 
-    private class Mover()
+    private class Mover( nint windowHandle)
     {
         #region Objects and variables
 
         private readonly Random r = new(Environment.TickCount);
-        private Point s = new();
+        private Point l = new(); // last mouse position
+        private Point s = new(); // first mouse position
         private int sW, sH;
+
+        private const int SW_SHOW = 5;
+        private const int SW_HIDE = 0;
 
         #endregion
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to function as a TimerCallback")]
+        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed to function as a TimerCallback")]
         internal void Move(object? state)
         {
+            var m = new Point(); // current mouse position
+
             if (s.X == 0 && s.Y == 0) // first trigger
             {
                 using var g = Graphics.FromHwnd(IntPtr.Zero);
                 var desktop = g.GetHdc();
-                sW = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPHORZRES);
+                
+                sW = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPHORZRES); // store screen resolution
                 sH = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
                 GetCursorPos(ref s); // store current position around which random moves will be performed
-                ShowWindow(GetConsoleWindow(), 6); // minimize console window
+                l = s; // last position = start position
+                ToggleWindow(false); // hide
             }
-            mouse_event((uint)(MouseFlags.Move | MouseFlags.Absolute), (int)(65535.0 * (s.X + ((r.Next(0, 2) * 2 - 1) * r.Next(1, 51)) + 1) / sW), (int)(65535.0 * (s.Y + ((r.Next(0, 2) * 2 - 1) * r.Next(1, 51)) + 1) / sH), 0, 0);
-            InvalidateRect(new IntPtr(GetWindowDC(IntPtr.Zero)), 0, true);
+            GetCursorPos(ref m); // store current position to check if user was active
+            if (m == l) // user inactive
+            {
+                // move mouse randomly (between -50 and +50 in both x and y direction)
+                mouse_event((uint)(MouseFlags.Move | MouseFlags.Absolute), (int)(65535.0 * (s.X + ((r.Next(0, 2) * 2 - 1) * r.Next(1, 51)) + 1) / sW), (int)(65535.0 * (s.Y + ((r.Next(0, 2) * 2 - 1) * r.Next(1, 51)) + 1) / sH), 0, 0);
+                InvalidateRect(new IntPtr(GetWindowDC(IntPtr.Zero)), 0, true); // prevent multiple cursor images
+            }
+            else // user active
+            {
+                l = m; // update last mouse position to current
+            }
+        }
+
+        internal void ToggleWindow(bool show)
+        {
+            ShowWindow(windowHandle, show ? SW_SHOW : SW_HIDE);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Mauzer
 
 Automatically minimizing console app that prevents a windows machine from locking or sleeping and prevents the current user from being marked as 'Away' by apps like Teams.
-This is done by calling SetThreadExecutionState and performing random mouse moves around a central point (i.e the current mouse position 3 secxonds after starting Mauzer).
+This is done by calling SetThreadExecutionState and performing random mouse moves around a central point (i.e the current mouse position 3 seconds after starting Mauzer).
+
+## Features
+
+* Moves the mouse randomly around the start position every 5 seconds
+* Only moves the mouse when the user has been inactive during the last interval
+* Prevents the system from locking or sleeping
+* Minimizes to the system tray (double click to show window)
